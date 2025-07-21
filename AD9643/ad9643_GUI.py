8a293c55ac027bc851d17d73d52ebeb77a3b34f3
shuralexx <shuralexxxx@gmail.com>
2025-07-21 15:14:42 +0800

    更新

--- AD9643/ad9643_GUI.py
+++ AD9643/ad9643_GUI.py
@@ -11,23 +11,31 @@ class ad9643:
         self.busy_status = 0
         self.connect_status = 0
         self.myserial = SerialAchieve()
-        self.regname = ['CONFIG0', 'CONFIG1', 'MUXSCH', 'MUXDIF', 'MUXSG0', 'MUXSG1', 'SYSRED', 'GPIOC', 'GPIOD', 'ID']
-        self.regaddr = [0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09]
+        #self.regname = ['CONFIG0', 'CONFIG1', 'MUXSCH', 'MUXDIF', 'MUXSG0', 'MUXSG1', 'SYSRED', 'GPIOC', 'GPIOD', 'ID']
+        #self.regaddr = [0x00,0x01,0x02,0x03,0x04,0x05,0x06,0x07,0x08,0x09]
         self.reg_mode = ['R/W', 'R/W', 'R/W', 'R/W', 'R/W', 'R/W', 'R/W', 'R/W', 'R/W', 'R']
         self.TD = [0,8,16,32,64,128,256,384]
         self.config = [0x0A, 0x83, 0x00, 0x00, 0x00, 0x00, 0x00, 0xff, 0x00, 0x8b]  # ADC 当前寄存器值
         self.config_default = [0x0A, 0x83, 0x00, 0x00, 0xff, 0xff, 0x00, 0xff, 0x00, 0x8b]  # ADC 内部默认寄存器配置
-        
+    
+
+    def crc(command_reg_write:list):
+        crc = 0
+
+        for byte in command_reg_write:
+            crc ^=byte
+        return crc
+    def crc2(command_rev:list):
+        crc =0
+        for i in range(0,len(command_rev)-1):
+            crc ^=command_rev[i]
+        return crc
+    
+
     def hand_shake(self):
         self.busy_status = 1
-        # while(1):
-        #     rx = self.myserial.ser.read()
-        #     if rx:
-        #         continue
-        #     else:
-        #         break 
-        command_handshake = [0x55, 0xAA, 0x0F, 0x65, 0x6E, 0x64, 0x45, 0x4E, 0x44]
-        command_rev = [0x55, 0xAA, 0xF0, 0x01, 0x65, 0x6E, 0x64, 0x45, 0x4E, 0x44]
+        command_handshake = [0xAA,0xFE,0x00,0x01,0x00,0x00,0x55]
+        command_rev = [0x55,0xFE,0x00,0x15,0x00,0x43,0x4D,0x33,0x34,0x33,0x32,0x5F,0x44,0x45,0x4D,0x4F,0x5f,0x56,0x31,0x31,0xE2]
         self.myserial.ser.write(command_handshake)
         rx = self.myserial.ser.readline()
 
@@ -46,24 +54,36 @@ class ad9643:
         return self
     
 
-    def ad9643_write_reg(self, addr, value):
+    def ad9643_write_reg(self, addr, para):
         """
         向ad9643指定寄存器地址写入数据
         :param addr: 1字节，寄存器地址
         :param value: 1字节，要写入的数据
         """
-        self.busy_status = 1
-        # ad9643写寄存器协议：[cmd1, cmd2, data]
-        command = [0x1B, 0x00, addr, value]
-        # 发送命令
-        self.myserial.ser.write(bytearray(command))
-        # 读取返回值，期望返回0x00
-        ret = self.myserial.ser.read(1)
-        if ret and ret[0] == 0x00:
-            print(f"成功写入ad9643寄存器0x{addr:02X}, 数据0x{value:02X}")
+        self.config[addr] = para
+        length = len(addr)+len(para)
+        length_bytes = length.to_bytes(2,byteorder='big')
+        command_reg_write = [0xAA, 0x1B, 0x00, 0x02,0x00,addr,para]
+        command_reg_Write =[0xAA, 0x1B, 0x00, 0x02,0x00,addr,para,self.crc(command_reg_write)]
+
+        command_rev = [0x55, 0x1B,0x00,0x01,0x00,0x00]
+        command_Rev = [0x55, 0x1B,0x00,0x01,0x00,0x00,self.crc2(command_rev)]
+
+        rx = []
+        self.myserial.ser.write(command_reg_Write)
+        for i in range(0, 7):
+            rx.append(self.myserial.ser.read())
+        if rx[6]==command_Rev[6]:
+            rx_asc_array = np.frombuffer(np.array(rx), dtype=np.uint8)
+            rx_asc = rx_asc_array.tolist()
+            if rx_asc == command_rev:
+                print('successfully config the device', addr, 'register')
+            else:
+                print('wrong received command')
         else:
-            print(f"写入ad9643寄存器失败，返回：{ret.hex() if ret else '无返回'}")
+            print('data wrong')
         self.busy_status = 0
+        return self 
 
 
     def ad9643_read_reg(self, addr):
@@ -73,21 +93,66 @@ class ad9643:
         :return: 读到的数据值
         """
         self.busy_status = 1
-        # ad9643读寄存器协议：[cmd1, cmd2, data]
-        command = [0x1A, 0x00, addr]
-        # 发送命令
-        self.myserial.ser.write(bytearray(command))
-        # 读取返回值，期望返回1字节寄存器数据
-        ret = self.myserial.ser.read(1)
-        if ret:
-            print(f"读取ad9643寄存器0x{addr:02X}，值=0x{ret[0]:02X}")
-            value = ret[0]
+        while(1):
+            rx = self.myserial.ser.read()
+            if rx:
+                continue
+            else:
+                break
+        data =0
+        command_reg_write = [0xAA, 0x1A,0x00,0x01,0x00,addr]
+        command_reg_Write = [0xAA, 0x1A,0x00,0x01,0x00,addr,self.crc(command_reg_write)]
+        command_rev = [0x55,0x1A,0x00, 0x01,0x00, data]
+
+        rx = []
+        self.myserial.ser.write(command_reg_Write)
+        for i in range(0, 7):
+            rx.append(self.myserial.ser.read())
+        if rx:
+            rx_asc_array = np.frombuffer(np.array(rx), dtype=np.uint8)
+            rx_asc = rx_asc_array.tolist()
+            if (rx_asc[0] == command_rev[0])and(rx_asc[1] == command_rev[1])and(rx_asc[2] == command_rev[2])and(rx_asc[3] == command_rev[3])and(rx_asc[4] == command_rev[4]):
+                print('The data is', rx_asc[5])
+            else:
+                print('wrong received command')
         else:
-            print(f"读取ad9643寄存器失败，无返回数据")
-            value = None
+            print('Wrong')
         self.busy_status = 0
-        return value
+        return self
     
+
+    def udp_Connect(self):
+        self.busy_status = 1
+        command_handshake = [0xAA,0x30,0x00,0x04,0x00,0x0A,0x20,0x1E,0x32,0x00]
+        command_rev = [0x55,0x30,0x00,0x01,0x00,0x00,0x62]
+        self.myserial.ser.write(command_handshake)
+        rx = self.myserial.ser.readline()
+        if rx:
+            rx_asc_array = np.frombuffer(rx, dtype=np.uint8)
+            rx_asc = rx_asc_array.tolist()
+            if rx_asc == command_rev:
+                print('Successfully configure local address')
+            else:
+                print('wrong received command')
+        else:
+            print('no data back!')
+        command_handshake1 = [0xAA,0x31,0x00,0x04,0x00,0x0A,0x20,0x1E,0x33,0x00]
+        command_rev1 = [0x55,0x31,0x00,0x01,0x00,0x00,0x63]
+        self.myserial.ser.write(command_handshake1)
+        rx = self.myserial.ser.readline()
+        if rx:
+            rx_asc_array = np.frombuffer(rx, dtype=np.uint8)
+            rx_asc = rx_asc_array.tolist()
+            if rx_asc == command_rev1:
+                print('Successfully configure pc address')
+            else:
+                print('wrong received command')
+        else:
+            print('no data back!')
+        self.connect_status = 1
+        self.busy_status = 0
+        return self
+
 
 
 class MainWindow(QWidget):
